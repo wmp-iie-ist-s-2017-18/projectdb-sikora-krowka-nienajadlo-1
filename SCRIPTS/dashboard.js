e31acb393f87e6894d5c6b8681566b8dfbf481be
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2019-04-14 21:37:04 +0200

    repaired settings

--- SCRIPTS/dashboard.js
+++ SCRIPTS/dashboard.js
@@ -7,8 +7,6 @@ $(document).ready(function () {
         if ($("#lastName").val() === "") $("#lastName").css("box-shadow", " 0px 0px 5px 0px rgba(255,0,0,0.86)");
         if ($("#position").val() === "") $("#position").css("box-shadow", " 0px 0px 5px 0px rgba(255,0,0,0.86)");
         if ($("#number").val() === "") $("#number").css("box-shadow", " 0px 0px 5px 0px rgba(255,0,0,0.86)");
-        if ($("#pesel").val() === "") $("#pesel").css("box-shadow", " 0px 0px 5px 0px rgba(255,0,0,0.86)");
-
         $("#dashboardSettingsNav").click();
         $("#dashboardHomeNav").add("#dashboardProjectsNav").add("#dashboardTeamNav").add("#adminSQL").hide();
     }
