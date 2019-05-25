9fbc5823a4edc308b13de8606792f0e07403c4c9
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2019-05-25 21:30:29 +0200

    project subpage description added

--- PHP/projectInfo.php
+++ PHP/projectInfo.php
@@ -16,7 +16,7 @@ catch (PDOException $e) {
 
 if($connection_status){
     try{
-        $stmt = $dbh->prepare("Select p.project_ID, p.name ,p.state , p.start ,p.finish ,p.description ,t.name  
+        $stmt = $dbh->prepare("Select p.project_ID, p.name ,p.state , p.start ,p.finish, p.budget ,p.description ,t.name  
         AS teamName  FROM project p, team_employee te ,team t where te.employee_ID = ".$_SESSION['id']." AND te.team_ID = p.team_ID GROUP BY p.name"); 
         $stmt->execute();
         $employeeArray = array();
@@ -25,11 +25,12 @@ if($connection_status){
         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
         foreach($stmt->fetchAll()as $k=>$v) {
                         echo ' <div class="tab-pane container" id="'.preg_replace('/\s+/', '', $v['name']).'">
-                            <h1 class="display-4"> '.$v['name'].'</h1>
-                            <h6>'.$v['state'].'</h6>
-                            <h6>'.$v['start'].'</h6>
-                            <h6>'.$v['finish'].'</h6>
-                            <h6>'.$v['teamName'].'</h6>
+                            <h1 class="display-4" style="text-align: center">'.$v['name'].'</h1>
+                            <h6 class="lead" style="padding: 1vh 2px;"><span style="font-size: 1.2rem; font-weight: bold;">Status: </span> '.$v['state'].'</h6>
+                            <h6 class="lead" style="padding: 1vh 2px;"><span style="font-size: 1.2rem; font-weight: bold;">Start: </span>'.$v['start'].'</h6>
+                            <h6 class="lead" style="padding: 1vh 2px;"><span style="font-size: 1.2rem; font-weight: bold;">Finish: </span> '.$v['finish'].'</h6>
+                            <h6 class="lead" style="padding: 1vh 2px;"><span style="font-size: 1.2rem; font-weight: bold;">Budget: </span>'.$v['budget'].'$</h6>
+                            <h6 class="lead" style="padding: 1vh 2px;"><span style="font-size: 1.2rem; font-weight: bold;">Team: </span>'.$v['teamName'].'</h6>
                             </div>';
         }
     }
