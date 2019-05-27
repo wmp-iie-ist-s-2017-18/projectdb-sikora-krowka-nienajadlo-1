677d203bc708b78cc27fba60232324337471a799
Adam Krówka <adamkrowka108@gmail.com>
2019-05-27 08:57:33 +0200

    refreshed database

--- DATABASE/projectmanager.sql
+++ DATABASE/projectmanager.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 25 Maj 2019, 19:39
--- Wersja serwera: 10.1.38-MariaDB
--- Wersja PHP: 5.6.40
+-- Czas generowania: 27 Maj 2019, 08:56
+-- Wersja serwera: 10.1.40-MariaDB
+-- Wersja PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -102,7 +102,7 @@ CREATE TABLE `employee` (
 
 INSERT INTO `employee` (`employee_ID`, `first_Name`, `last_Name`, `email`, `password`, `position`, `activated`, `tel_number`, `activation_code`, `company_ID`) VALUES
 (66, 'Adam', 'Krówka', 'adamkrowka108@gmail.com', '$2y$10$NAwNjF7nPyiZM26uUI1zCO8mf3.ZMhHAsDgVcwv78XGn0NYdA63hC', 'Full-Stack Developer', 1, 537399743, 2147483647, 1),
-(67, 'Dawid', 'Nienajadło', 'dawidnienajadlo96@gmail.com', '$2y$10$9QWF6K6KKY06OvxXAK/ec.rg7jEJUge3MYl9D8E0WXuKn1y1FDOyu', 'Project Manager', 1, 516804066, 1037142154, 1);
+(67, 'Dawid', 'Nienajadło', 'admin@gmail.com', '$2y$10$9QWF6K6KKY06OvxXAK/ec.rg7jEJUge3MYl9D8E0WXuKn1y1FDOyu', 'Admin', 1, 516804066, 1037142154, 1);
 
 -- --------------------------------------------------------
 
@@ -139,7 +139,9 @@ CREATE TABLE `project` (
 --
 
 INSERT INTO `project` (`project_ID`, `name`, `state`, `start`, `finish`, `team_ID`, `description`, `budget`) VALUES
-(2, 'NextProject', 'Active', '2019-04-01', '2019-07-26', 2, 'sadas', 150000);
+(2, 'NextProject', 'Active', '2019-04-01', '2019-07-26', 2, 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.', 150000),
+(3, 'new', 'Active', '2019-05-26', '2019-05-27', 1, 'dsadsa', 200000),
+(4, 'Nowy projekt', 'Active', '2019-05-27', '2019-05-27', 2, 'Projekt stworzony na potrzeby testu.', 100200);
 
 -- --------------------------------------------------------
 
@@ -153,20 +155,6 @@ CREATE TABLE `projectchat` (
   `employee_ID` int(11) NOT NULL,
   `message` varchar(255) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
---
--- Zrzut danych tabeli `projectchat`
---
-
-INSERT INTO `projectchat` (`Chat_ID`, `team_ID`, `employee_ID`, `message`) VALUES
-(1, 2, 66, 'Hello Hello'),
-(2, 2, 66, 'Jestem adam i popsułem czat'),
-(3, 2, 66, 'i kaszle na Dawida komputer'),
-(4, 2, 66, 'i mówie, że się zasłaniam'),
-(5, 1, 66, 'Jestem Adam i nadal psuje czat'),
-(6, 1, 66, 'i w dodatku sapie'),
-(7, 1, 66, 'i lubie wazeline'),
-(8, 2, 67, 'Dobra powiedzmy, że się nie gniewam, ale żeby to było ostatni raz!');
 
 -- --------------------------------------------------------
 
@@ -186,7 +174,8 @@ CREATE TABLE `team` (
 
 INSERT INTO `team` (`team_ID`, `name`, `leader_ID`) VALUES
 (1, 'FirstTeam', 66),
-(2, 'Second Team', 67);
+(2, 'Second Team', 67),
+(3, 'Testowy Team', 67);
 
 -- --------------------------------------------------------
 
@@ -207,7 +196,8 @@ INSERT INTO `team_employee` (`employee_ID`, `team_ID`) VALUES
 (67, 2),
 (66, 2),
 (66, 2),
-(66, 2);
+(66, 2),
+(67, 3);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -296,7 +286,7 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT dla tabeli `project`
 --
 ALTER TABLE `project`
-  MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT dla tabeli `projectchat`
@@ -308,7 +298,7 @@ ALTER TABLE `projectchat`
 -- AUTO_INCREMENT dla tabeli `team`
 --
 ALTER TABLE `team`
-  MODIFY `team_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `team_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ograniczenia dla zrzutów tabel
